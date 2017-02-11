0f82ed668e1bad2c759b13e0fd0bb3133fc9d5bf
Juan Carlos Perez Jimenez <jcpj109@gmail.com>
2017-02-11 16:02:29 -0600

    Metodo Java
    Ejemplos de uso de datos sin parametros y sin retorno en java

---
+++ Tarea1_3.java
@@ -0,0 +1,13 @@
+public class Tarea1_3 {
+	public static void main(String[]args){
+		circulo();
+	}
+	public static void circulo(){
+		double pi, r, a;
+		pi=3.141592654;
+		r=5;
+		a=pi*(r*r);
+		System.out.println("El are del circulo es = "+a);
+	}
+
+}
