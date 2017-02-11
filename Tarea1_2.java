13159e332b55c9fba5b208b54f300872ae594e67
JuanPRZ <jcpj109@gmail.com>
2017-02-11 15:48:56 -0600

    Metodo1 java
    ejemplo de uso de metodos sin retono y sin parametros en java

---
+++ Tarea1_2.java
@@ -0,0 +1,11 @@
+public class Tarea1_2 {
+	public static void main(String []args ){
+		rectangulo();
+	}
+	public static void rectangulo(){
+		int base=3;
+		int altura=6;
+		int area= base*altura;
+		System.out.println("El area del rectangulo es = "+ area);	
+	}
+}
