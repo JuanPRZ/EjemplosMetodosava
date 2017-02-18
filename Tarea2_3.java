8eeefccb58f54fb90b99fd3b2bb2d07ad73d9f3b
Juan Carlos Perez Jimenez <jcpj109@gmail.com>
2017-02-18 15:35:57 -0600

    Create Tarea2_3.java

---
+++ Tarea2_3.java
@@ -0,0 +1,11 @@
+public class Tarea2_3 {
+	public static void main(String[]args){
+		circulo(3.141592654,5);
+	}
+	public static void circulo(double pi,double radio){
+		double area;
+		area=pi*(radio*radio);
+		System.out.println("El area del circulo es = "+ area);
+	}
+
+}
