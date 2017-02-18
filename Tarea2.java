0d8fefaa3dbc25a64db3e5f39c6db0562985efd9
Juan Carlos Perez Jimenez <jcpj109@gmail.com>
2017-02-18 15:34:15 -0600

    Create Tarea2.java

---
+++ Tarea2.java
@@ -0,0 +1,12 @@
+public class Tarea2 {
+	public static void main(String []args){
+	
+	rectangulo(5,2);
+	}
+	public static void rectangulo(int base, int altura){
+
+		int area = base*altura;
+		System.out.println("El area del rectangulo es = "+area);
+	}
+	
+}
