f9316bc02a25d6f75afc195c2ad459478e3b6460
Juan Carlos Perez Jimenez <jcpj109@gmail.com>
2017-02-18 15:34:48 -0600

    Create Tarea2_2.java

---
+++ Tarea2_2.java
@@ -0,0 +1,10 @@
+public class Tarea2_2 {
+	public static void main(String []args){
+		cuadrado(4);
+	}
+	public static void cuadrado(int lado){
+		int area=lado*lado;
+		System.out.println("El area del cuadrado es = "+area);
+	}
+
+}
