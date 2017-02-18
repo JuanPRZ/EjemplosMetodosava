8f1af20ae530efd297c354c3132cdda539555292
Juan Carlos Perez Jimenez <jcpj109@gmail.com>
2017-02-18 15:31:54 -0600

    Create Terea1.java

---
+++ Terea1.java
@@ -0,0 +1,14 @@
+public class Terea1 {
+
+	
+	public static void main(String[] args) {
+		cuadrado();
+	}
+		public static void cuadrado(){
+		int lado=4;
+		int area=lado*lado;
+		System.out.println("El area del cuadrado es = "+area);
+		
+	}
+
+}
