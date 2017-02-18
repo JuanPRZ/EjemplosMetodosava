3e80ee32d72238f4a0af7ed13134c7ba767b4038
Juan Carlos Perez Jimenez <jcpj109@gmail.com>
2017-02-18 15:36:40 -0600

    Create Tarea2_4.java

---
+++ Tarea2_4.java
@@ -0,0 +1,14 @@
+public class Tarea2_4 {
+	public static void main(String[]args){
+		nummayor(3,5);
+	}
+	public static void nummayor(int num1, int num2){
+		if (num1>num2){
+			System.out.println("El numero mayor es "+ num1);	
+		}
+		else{
+			System.out.println("EL numero mayor es "+ num2);
+		}
+	}
+
+}
