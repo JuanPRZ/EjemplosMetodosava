637894bbea15b6aa598745eda65a41d4dac87170
Juan Carlos Perez Jimenez <jcpj109@gmail.com>
2017-02-18 15:33:33 -0600

    Create Tarea1_4.java

---
+++ Tarea1_4.java
@@ -0,0 +1,17 @@
+public class Tarea1_4 {
+	public static void main(String[]args){
+		numayor();
+	}
+	public static void numayor(){
+		int num1=3;
+		int num2=5;
+		if (num1>num2){
+			System.out.println("El numero mayor es "+ num1);	
+		}
+		else{
+			System.out.println("EL numero mayor es "+ num2);
+		}
+	}
+	
+
+}
